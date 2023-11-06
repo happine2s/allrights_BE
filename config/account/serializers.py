e8b5ffafe111c926f64313f5e1086db863a5a181
happine2s <happine2s@sookmyung.ac.kr>
2023-11-06 20:37:25 +0900

    [fix] drf를 위한 serializers 사용

--- config/account/serializers.py
+++ config/account/serializers.py
@@ -1,7 +1,16 @@
 from .models import User
 from rest_framework import serializers
 
-class SignupSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['userid', 'username', 'password']
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            userid = validated_data['userid'],
+            username = validated_data['username'],
+            password=validated_data['password']
+        )
+
+        return user
