45516c14b40e99abe4fa4446157085e84b0add3b
happine2s <happine2s@sookmyung.ac.kr>
2023-11-06 20:38:14 +0900

    [feat] 로그인/로그아웃 구현

--- config/account/urls.py
+++ config/account/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('signup/',signup.as_view()),
+    path('signin/',signin.as_view()),
+    path('signout/',signout.as_view()),
 ]
--- config/account/views.py
+++ config/account/views.py
@@ -4,11 +4,13 @@ from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib import auth
+from django.contrib.auth import authenticate
 
 
 class signup(APIView):
     def post(self, request):
-        serializer=SignupSerializer(data=request.data)
+        serializer=UserSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
@@ -23,4 +25,33 @@ class signup(APIView):
                 "msg":"failed"
             }
             return Response(data, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        
+
+class signin(APIView):
+    def post(self, request):
+        serializer=UserSerializer(data=request.data)
+
+        userid=request.data['userid']
+        password=request.data['password']
+        user=authenticate(request,userid=userid, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            data={
+                "msg":"ok",
+                "userid":userid
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            # 로그인 실패 시
+            data={
+                "msg":"failed"
+            }
+            return Response(data,status=status.HTTP_404_NOT_FOUND)
+
+class signout(APIView):
+    def post(self, request):
+        auth.logout(request)
+        data={
+            "msg":"ok"
+        }
+        return Response(data, status=status.HTTP_200_OK)
