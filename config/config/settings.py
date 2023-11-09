0b0e5ca70df6ad2513acf85b3130f74c8bab639e
happine2s <happine2s@sookmyung.ac.kr>
2023-11-10 00:24:27 +0900

    [fix] 프론트 포트 수정

--- config/config/settings.py
+++ config/config/settings.py
@@ -147,5 +147,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8000' ,'http://localhost:5137']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8000' ,'http://localhost:5173']
 CORS_ALLOW_CREDENTIALS = True
