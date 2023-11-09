d59aa512841ddbd28b033ff743751601c0e7b27a
newoceanwave <leeseohyun02@naver.com>
2023-11-09 10:12:06 +0900

    [fix] 음악 업로드 수정

--- config/music/urls.py
+++ config/music/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import MusicList, MusicDetail, MusicSearch, download_music
+from .views import MusicList, MusicDetail, MusicSearch, download_music, MusicUpload
 
 urlpatterns = [
     path('', MusicList.as_view()),
+    path('upload/', MusicUpload.as_view()),
     path('<int:pk>/', MusicDetail.as_view()),
     path('search/', MusicSearch.as_view(), name='music-search'),
     path('download/<int:music_id>/', download_music, name='download-music'),
