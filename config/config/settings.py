cd25384787528830ae7effdc60291ab05ffff8c0
happine2s <happine2s@sookmyung.ac.kr>
2023-11-10 10:20:35 +0900

    [fix] 음악 게시물 파일 업로드 오류 해결

--- config/config/settings.py
+++ config/config/settings.py
@@ -137,7 +137,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 AUTH_USER_MODEL='account.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ]
-}
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+     )
+ }
--- config/music/models.py
+++ config/music/models.py
@@ -50,13 +50,11 @@ class Music(models.Model):
     genre = models.CharField(max_length=100, choices=GENRE_TYPES)
     instruments = models.CharField(max_length=100, choices=INSTRUMENTS_TYPES)
     mood = models.CharField(max_length=100, choices=MOOD_TYPES)
-    length = models.IntegerField(null=True, blank=True)
+    length = models.IntegerField(default=0)
     description = models.TextField()
     upload_date = models.DateTimeField(default=timezone.now)  # 업로드 날짜 및 시간
     downloads = models.PositiveIntegerField(default=0)  # 다운로드 횟수
-    music_image = models.ImageField(upload_to='image/', blank=True)
-
-
+    music_image = models.ImageField(upload_to='image/',blank=True,null=True)
     author=models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     liker=models.ManyToManyField(User,related_name='like_music',default=[],blank=True)
 
