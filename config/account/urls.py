aa30a7cd1ef8d38adb0e23768432497607503e76
happine2s <happine2s@sookmyung.ac.kr>
2023-11-11 01:09:38 +0900

    [feat] jwt 인증 회원 기능 구현

--- config/account/urls.py
+++ config/account/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('token/refresh/', TokenRefreshView.as_view()), # jwt 재발급
     path('signup/',signup.as_view()),
+    path('auth/',auth.as_view()),
     path('signin/',signin.as_view()),
     path('signout/',signout.as_view()),
     path('password/',update_password.as_view()),
--- config/config/settings.py
+++ config/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'corsheaders',
+    'rest_framework_simplejwt',
 
     'account',
     'music',
@@ -142,10 +144,47 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 AUTH_USER_MODEL='account.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PARSER_CLASSES': [
+    'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    ]
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8000' ,'http://localhost:5137']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8000' ,'http://localhost:5173']
 CORS_ALLOW_CREDENTIALS = True
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5), # ACCESS Token의 유효기간
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=50), # Refresh 토큰의 유효기간 
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(hours=1),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
